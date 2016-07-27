a0f7be4deefa6cc47af2b2546a8050012d25724d
Eubene Sa <eubene.sa@gmail.com>
2016-07-27 14:54:00 -0700

    Finish ex3

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,7 +1,11 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult(prod, num1, ...nums) { // 2 or more
+  prod *= num1;
+  if (nums.length === 0) {
+    return prod;
+  } else {
+    return mult(prod, ...nums);
+  }
 }
 
-mult(3,4,5);	// 60
-
-mult(3,4,5,6);	// Oops!
+console.log(mult(3, 4, 5)); // 60
+console.log(mult(3, 4, 5, 6)); // 360
